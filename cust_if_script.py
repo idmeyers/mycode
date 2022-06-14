ee6d7b84154ecc20953738147fce95b41adf75aa
idmeyers <idmeyers@users.noreply.github.com>
2022-06-14 20:45:05 +0000

    while looping

--- cust_if_script.py
+++ cust_if_script.py
@@ -5,21 +5,33 @@
 
 message = 'Your score means '
 
+def check_is_digit(input_str):
+    if input_str.strip().isdigit():
+        print("User input is Number")
+        return True
+    else:
+        print("User input is string")
+        return False
 # wrap connection in a float() to accept decimals as numbers
-connection = float(input("What is your score?"))
-
+score = (input("What is your score?"))
+if check_is_digit(score):
+    score = float(score) 
 # if input value was higher or equal to 25
-if connection >= 90:
-    message = message + 'that you received an A.'
-elif connection >= 80:
-    message = message + 'that you received a B.'
-elif connection >= 70:
-    message = message + 'that you received a C.'
-elif connection >= 60:
-    message = message + 'that you received a D.'
-elif connection >= 0:
-    message = message + 'that you failed and need to study more.'
+    if score >= 90:
+        message = message + 'that you received an A.'
+    elif score >= 80:
+        message = message + 'that you received a B.'
+    elif score >= 70:
+        message = message + 'that you received a C.'
+    elif score >= 60:
+        message = message + 'that you received a D.'
+    elif score >= 0:
+        message = message + 'that you failed and need to study more.'
+    else:
+        message = message + 'that you did not actually take a test.'
+
 else:
     message = message + 'that you did not actually take a test.'
+
 print(message)
 
