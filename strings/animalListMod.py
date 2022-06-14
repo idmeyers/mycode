3f49914b6dbe7666af3b819e04ded14faae1e581
idmeyers <idmeyers@users.noreply.github.com>
2022-06-14 17:19:52 +0000

    copying local files with python

---
+++ strings/animalListMod.py
@@ -0,0 +1,11 @@
+#make your own list of animals
+# Cap each one of the animals names
+# and put in a new list 
+
+a = ["cat","dog"]
+pets = " ".join(a)
+pets2 = pets.title()
+print(pets2)
+b = pets2.split()
+print(b)
+
