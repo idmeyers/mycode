fcb9fe21333eb6ce537153c3e8bfc7f5e621227d
idmeyers <idmeyers@users.noreply.github.com>
2022-06-14 13:53:14 +0000

    manipulating lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+my_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# display the first item in the list
+print("The first IP in this list (IP): " + my_list[3] )
+print("The second IP in this list (IP): " + my_list[4] )
+# display the second item in the list
+print("The ports in the list (port): %s, %s and %s" % ( (my_list[0]), (my_list[1]), (my_list[2]))  )
+
+# display the third item in the list
+print("The last item in the list (state): " + my_list[5] )
+
+
